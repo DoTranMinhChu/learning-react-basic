214c5a0583f0df1278901ae0e3bb4d4c37cd0ab4
Minh Chu <dotranminhchu@gmail.com>
2022-03-02 15:18:53 +0700

    JSX update

--- src/views/example/MyClassComponent.js
+++ src/views/example/MyClassComponent.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 
-class MyClassComponents extends React.Component{
-
-    render(){
-        const name = 'MinhChu';
-        return(
-            <div>This is Class Component {name}</div>
-        );
-    }
-
+class MyClassComponents extends React.Component {
+  render() {
+    const name = "MinhChu";
+    return (
+      <div>
+        <div>
+          {"Screen: " + name}
+          {console.log("Console : " + name)}
+        </div>
+        This is Class Component {name}
+      </div>
+    );
+  }
 }
 
 export default MyClassComponents;
